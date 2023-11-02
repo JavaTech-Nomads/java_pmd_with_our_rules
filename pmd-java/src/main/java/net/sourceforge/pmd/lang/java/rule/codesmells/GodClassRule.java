918d17318a3f2e55594103e230acfd81e1a8dc5e
Juanita Rodelo <juanitarodeloo@gmail.com>
2023-11-02 17:45:18 -0400

    Add God class rule and example file to test with

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codesmells/GodClassRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codesmells/GodClassRule.java
@@ -29,21 +29,23 @@ public class GodClassRule extends AbstractJavaRulechainRule {
     /**
      * Very high threshold for WMC (Weighted Method Count). See: Lanza. Object-Oriented Metrics in Practice. Page 16.
      */
-    private static final int WMC_VERY_HIGH = 47;
-
+    private static final int WMC_VERY_HIGH = 3; // changed to a weird metric to ensure that it goes into if statement //-447
+    //look into loc metric that will dynamically set these metrics
     /**
      * Few means between 2 and 5. See: Lanza. Object-Oriented Metrics in Practice. Page 18.
      */
-    private static final int FEW_ATFD_THRESHOLD = 5;
+    private static final int FEW_ATFD_THRESHOLD = 1; // changed to a weird metric to ensure that it goes into if statement
+
 
     /**
      * One third is a low value. See: Lanza. Object-Oriented Metrics in Practice. Page 17.
      */
-    private static final double TCC_THRESHOLD = 1.0 / 3.0;
+    private static final double TCC_THRESHOLD = (1.0 / 3.0); // changed to a weird metric to ensure that it goes into if statement
+
 
 
     public GodClassRule() {
-        super(ASTClassOrInterfaceDeclaration.class);
+        super(ASTClassOrInterfaceDeclaration.class); 
     }
 
 
@@ -52,17 +54,16 @@ public class GodClassRule extends AbstractJavaRulechainRule {
         if (!MetricsUtil.supportsAll(node, WEIGHED_METHOD_COUNT, TIGHT_CLASS_COHESION, ACCESS_TO_FOREIGN_DATA)) {
             return data;
         }
+        
+        int wmc = MetricsUtil.computeMetric(WEIGHED_METHOD_COUNT, node); //measures the complexity of a class (cyclomatic complexity)
+        double tcc = MetricsUtil.computeMetric(TIGHT_CLASS_COHESION, node); //measures the cohesion of a class
+        int atfd = MetricsUtil.computeMetric(ACCESS_TO_FOREIGN_DATA, node); //measures the number of attributes from unrelated classes that are accessed directly or through accessor methods
 
-        int wmc = MetricsUtil.computeMetric(WEIGHED_METHOD_COUNT, node);
-        double tcc = MetricsUtil.computeMetric(TIGHT_CLASS_COHESION, node);
-        int atfd = MetricsUtil.computeMetric(ACCESS_TO_FOREIGN_DATA, node);
-
+        System.out.println("outside if");
         if (wmc >= WMC_VERY_HIGH && atfd > FEW_ATFD_THRESHOLD && tcc < TCC_THRESHOLD) {
-
-            /*addViolation(data, node, new Object[] {wmc,
-                                                   StringUtil.percentageString(tcc, 3),
-                                                   atfd, });*/
-            asCtx(data).addViolation(node, new Object[] {wmc, StringUtil.percentageString(tcc, 3), atfd, });
+            System.out.println("in if");
+            addViolation(data, node, new Object[] {wmc, StringUtil.percentageString(tcc, 3), atfd, });
+            //asCtx(data).addViolation(node, new Object[] {wmc, StringUtil.percentageString(tcc, 3), atfd, });
         }
         return data;
     }
